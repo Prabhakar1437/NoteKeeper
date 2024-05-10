70236edd7af415c6cea7d9c799b515d81bc9e32b
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-05-10 16:22:05 +0530

    Done

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+import plusIcon from "../../assets/plus.png";
+
+import "./Sidebar.css";
+
+export default function Sidebar(props) {
+  const colors = ["#fe9b72", "#fec971", " #00d4fe", "#b693fd", "#e4ee91"];
+
+  const [listOpen, setListOpen] = useState(false);
+
+  return (
+    <div className="sidebar">
+      <img src={plusIcon} alt="Add" onClick={() => setListOpen(!listOpen)} />
+      <ul className={`sidebar_list ${listOpen ? "sidebar_list_active" : ""}`}>
+        {colors.map((item, index) => (
+          <li
+            key={index}
+            className="sidebar_list_item"
+            style={{ backgroundColor: item }}
+            onClick={() => props.addNote(item)}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
